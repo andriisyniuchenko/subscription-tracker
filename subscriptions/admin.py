cab02694effcbe4a7984ad1ed56e9f9e9d3d6bce
AndriiSyniuchenko <andriisyniuchenko@gmail.com>
2026-03-29 23:54:20 -0700

    Improve admin panel with filters, search, and display

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -2,4 +2,28 @@ from django.contrib import admin
 from .models import Subscription
 
 
-admin.site.register(Subscription)
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'user',
+        'price',
+        'billing_cycle',
+        'is_active',
+        'start_date',
+        'created_at',
+    )
+
+    list_filter = (
+        'billing_cycle',
+        'is_active',
+        'start_date',
+    )
+
+    search_fields = (
+        'name',
+        'user__username',
+    )
+
+    ordering = ('-created_at',)
