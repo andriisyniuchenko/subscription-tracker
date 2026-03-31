364e2fda7d968edf1c0aa1b5da98345e62e23175
AndriiSyniuchenko <andriisyniuchenko@gmail.com>
2026-03-30 20:56:55 -0700

    Add update subscription functionality

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import subscription_list, create_subscription
+from .views import subscription_list, create_subscription, update_subscription
 
 
 urlpatterns = [
     path('', subscription_list, name='subscription_list'),
     path('create/', create_subscription, name='create_subscription'),
+    path('update/<int:pk>/', update_subscription, name='update_subscription'),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Subscription
 from .forms import SubscriptionForm
 from django.contrib.auth.decorators import login_required
@@ -26,5 +26,26 @@ def create_subscription(request):
         form = SubscriptionForm()
 
     return render(request, 'subscriptions/create.html', {
+        'form': form
+    })
+
+
+@login_required
+def update_subscription(request, pk):
+    subscription = get_object_or_404(
+        Subscription,
+        id=pk,
+        user=request.user  # 🔥 важливо! захист
+    )
+
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST, instance=subscription)
+        if form.is_valid():
+            form.save()
+            return redirect('subscription_list')
+    else:
+        form = SubscriptionForm(instance=subscription)
+
+    return render(request, 'subscriptions/update.html', {
         'form': form
     })
